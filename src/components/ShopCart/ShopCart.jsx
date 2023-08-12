38f9e05d5c8c0ebf989ee3dd3637c8ba2b06e197
Pouya Barari Gelyard <93615101+pouyaBG@users.noreply.github.com>
2023-08-12 15:26:21 +0330

    make shop

--- src/components/ShopCart/ShopCart.jsx
+++ src/components/ShopCart/ShopCart.jsx
@@ -2,18 +2,19 @@ import React, { useContext } from "react";
 import { CardContext } from "../../context/CardContextProvider";
 import OneShpCart from "../shared/OneShopCart/OneShpCart";
 import { Link } from "react-router-dom";
+import styles from "./ShopCart.module.css";
 
 const ShopCart = () => {
   const { state, dispatch } = useContext(CardContext);
   return (
-    <div>
-      <div style={{ marginTop: "100px" }}>
+    <div className={styles.container}>
+      <div className={styles.cartContainer}>
         {state.selectedItems.map((item, id) => (
           <OneShpCart key={id} data={item} />
         ))}
       </div>
       {state.itemsCounter > 0 && (
-        <div>
+        <div className={styles.payments}>
           <div>
             <p>
               <span>Total Items:</span>
@@ -21,21 +22,35 @@ const ShopCart = () => {
             </p>
             <p>
               <span>Total Payments:</span>
-              {state.total}
+              {state.totalItems} $
             </p>
           </div>
-          <div>
-            <button onClick={() => dispatch({ type: "CHECKOUT" })}>
+          <div className={styles.buttonContainer}>
+            <button
+              className={styles.checkout}
+              onClick={() => dispatch({ type: "CHECKOUT" })}
+            >
               CHECKOUT
             </button>
-            <button onClick={() => dispatch({ type: "CLEAR" })}>CLEAR</button>
+            <button
+              className={styles.clear}
+              onClick={() => dispatch({ type: "CLEAR" })}
+            >
+              CLEAR
+            </button>
           </div>
         </div>
       )}
       {state.checkout && (
-        <div>
-          <h3>CheckedOut sucses</h3>
-          <Link to={"/"}>buy more?</Link>
+        <div className={styles.complete}>
+          <h3>Checked out successfully</h3>
+          <Link to="/">Buy More</Link>
+        </div>
+      )}
+      {state.itemsCounter === 0 && !state.checkout && (
+        <div className={styles.complete}>
+          <h3>Want to buy?</h3>
+          <Link to="/">Go to shop</Link>
         </div>
       )}
     </div>
